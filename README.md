f527fb24e5f6cd60bf8ee4f3b6b794505c0190f0
Sedrick Gael <kanangamansedrickgael@gmail.com>
2021-09-20 01:03:43 +0000

    create model

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # directory
  Teacher Directory
+
+
+
+# CREATE and ACTIVATE VIRTUAL ENV 
+```bat
+cd ..
+python -m venv venv
+.\venv\scripts\activate
+```
+
+# INSTALL REQUIREMENTS
+```bat
+cd directory
+pip install -r requirements.txt
+```
+
+# RUN SERVER
+```BAT
+python manage.py runserver
+```
--- directory/teacher/models.py
+++ directory/teacher/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+
+class Subject(models.Model):
+    """Model definition for Subject."""
+
+    # TODO: Define fields here
+    subject_name = models.CharField(max_length=250)
+    description = models.TextField(null=True, blank=True)
+
+
+    # standar
+
+    statut = models.BooleanField()
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Meta definition for Subject."""
+
+        verbose_name = 'Subject'
+        verbose_name_plural = 'Subjects'
+
+    def __str__(self):
+        """Unicode representation of Subject."""
+        return self.subject_name
+
+
+
+class Teacher(models.Model):
+    """Model definition for Teacher."""
+
+    # TODO: Define fields here
+    last_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=254)
+    profile_picture = models.ImageField(upload_to="Teachers/Profile_Picture", default="default.png")
+    phone_number = models.CharField(max_length=50)
+    room_number = models.CharField(max_length=10)
+    subjects_tauch = models.ManyToManyField(Subject, blank=True)
+
+
+    statut = models.BooleanField()
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Meta definition for Teacher."""
+
+        verbose_name = 'Teacher'
+        verbose_name_plural = 'Teachers'
+
+    def __str__(self):
+        """Unicode representation of Teacher."""
+        return self.last_name + ' ' + self.last_name + '(' + self.email + ')'
+
