fb3cb1fb8656d4417f551da90a333c18d54d3fbe
Sédrick Gael KOUAGNI <kanangamansedrickgael@gmail.com>
2021-09-20 12:48:25 -0700

    begin vue js

--- directory/dashboard/urls.py
+++ directory/dashboard/urls.py
@@ -3,8 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login, name='login'),
     path('subjects', views.subjects, name='subjects'),
     path('profil', views.profil, name='profil'),
     path('add_subject', views.add_subject, name='add_subject'),
+
+    ########################################################
+    ############## AUTHENTICATION ####################################
+    ########################################################
+    path('login', views.login, name='login'),
+    path('deconnexion', views.deconnexion, name='logout'),
+
+    ########################################################
+    ############## POST ####################################
+    ########################################################
+    path('post_login', views.islogin, name='post_login'),
 ]
--- directory/dashboard/views.py
+++ directory/dashboard/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect 
 from . import models
-# Create your views here.
+from django.contrib.auth import authenticate, login as login_request, logout
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User 
+from django.contrib.auth.decorators import login_required
 
+# default page (all teacher)
 def index(request):
     datas = {
 
     }
     return render(request, 'pages/teacher.html', datas)
 
-
+#  user login 
 def login(request):
     datas = {
 
@@ -16,15 +21,15 @@ def login(request):
     return render(request, 'pages/login.html', datas)
 
 
-
+# show all subjects
 def subjects(request):
     datas = {
 
     }
     return render(request, 'pages/subjects.html', datas)
 
-
-
+#  teacher profil view
+@login_required(login_url = 'login')
 def profil(request):
     datas = {
 
@@ -32,10 +37,68 @@ def profil(request):
     return render(request, 'pages/profil.html', datas)
 
 
-
+# page for add subect
+@login_required(login_url = 'login')
 def add_subject(request):
     datas = {
 
     }
     return render(request, 'pages/add-subjects.html', datas)
+
+# LOGOUT VIEW
+def deconnexion(request):
+    logout(request)
+    return redirect('login')
+
+#######################################################################
+#######################################################################
+############################ POST VIEWS ###############################
+#######################################################################
+#######################################################################
+
+
+def islogin(request):
+
+    postdata = json.loads(request.body.decode('utf-8'))
+        
+    # name = postdata['name']
+
+    username = postdata['username']
+    password = postdata['password']
+
+    isSuccess = False
+    u_type = ''
+    try:
+        if '@' in username:
+            user = authenticate(email=username, password=password)
+            utilisateur = User.objects.get(email=username)
+            print(username)
+        else:
+            user = authenticate(username=username, password=password)
+            utilisateur = User.objects.get(username=username)
+            
+        if user is not None and user.is_active:
+            print("user is login")
+            isSuccess = True
+            login_request(request, user)
+            datas = {
+                'success':True,
+                'message':'Vous êtes connectés!!!',
+            }
+            return JsonResponse(datas,safe=False) # page si connect
+                
+        else:
+            data = {
+                'success':False,
+                'message':'Vos identifiants ne sont pas correcte',
+            }
+            return JsonResponse(data, safe=False)
+    except Exception as e:
+        print(e)
+        data = {
+            'success':False,
+            'message':"Merci de verifier vos informations d'authentification",
+        }
+        return JsonResponse(data, safe=False)
+
 
