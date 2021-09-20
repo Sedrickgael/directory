2245607bde935b7e2902f67a44fd441c19bddf45
Sédrick Gael KOUAGNI <kanangamansedrickgael@gmail.com>
2021-09-20 10:32:46 -0700

    template integration

--- directory/dashboard/urls.py
+++ directory/dashboard/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='home')
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('subjects', views.subjects, name='subjects'),
+    path('profil', views.profil, name='profil'),
+    path('add_subject', views.add_subject, name='add_subject'),
 ]
--- directory/dashboard/views.py
+++ directory/dashboard/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+
+def index(request):
+    datas = {
+
+    }
+    return render(request, 'pages/teacher.html', datas)
+
+
+def login(request):
+    datas = {
+
+    }
+    return render(request, 'pages/login.html', datas)
+
+
+
+def subjects(request):
+    datas = {
+
+    }
+    return render(request, 'pages/subjects.html', datas)
+
+
+
+def profil(request):
+    datas = {
+
+    }
+    return render(request, 'pages/profil.html', datas)
+
+
+
+def add_subject(request):
+    datas = {
+
+    }
+    return render(request, 'pages/add-subjects.html', datas)
+
